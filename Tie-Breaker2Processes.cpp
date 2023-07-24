e3fa184a36fe0bc7327b92bbc3a59c090d899eee
Marko Rabat <rm200196d@student.etf.bg.ac.rs>
2023-07-24 22:46:18 +0200

    error fix

--- Tie-Breaker2Processes.cpp
+++ Tie-Breaker2Processes.cpp
@@ -11,6 +11,9 @@ std::atomic<int> last = { 1 };
 bool t1_working = true;
 bool t2_working = true;
 
+bool t1in = false;
+bool t2in = false;
+
 int getRandInt(int x, int y) {
 	return std::abs(x) + (std::rand() % std::abs((y - x)));
 }
@@ -25,13 +28,22 @@ void f1() {
 		while (in2.load(std::memory_order_seq_cst) &&			// while (in2 and last == 1) skip;
 			   last.load(std::memory_order_seq_cst) == 1)
 			/* skip */ Sleep(getRandInt(50, 300));
+		
+		t1in = true;
 
 		for (int i = 0; i < getRandInt(100, 1000); ++i) /* some work here */;	// critical section
-		std::cout << "Critical section t1" << std::endl;
+		for (int i = 0; i < 10; ++i) {
+			std::cout << "Critical section t1" << std::endl;
+			if (t1in && t2in)
+				std::cout << "Doesn't work!!!" << std::endl;
+		}
+
+		t1in = false;
 
 		in1.store(false);										// exit protocol
 
 		for (int i = 0; i < getRandInt(100, 1000); ++i) /* some work here */;	// non-critical section
+		for (int i = 0; i < 5; ++i)
 		std::cout << "Non-critical section t1" << std::endl;
 	}
 }
@@ -41,19 +53,28 @@ void f2() {
 	while (t2_working) {
 																// entry protocol
 		in2.store(true, std::memory_order_seq_cst);
-		last.store(1, std::memory_order_seq_cst);
+		last.store(2, std::memory_order_seq_cst);
 
 																// <await(!in1 or last == 1);>
 		while (in1.load(std::memory_order_seq_cst) &&			// while (in1 and last == 2) skip;
 			   last.load(std::memory_order_seq_cst) == 2)
 			/* skip */ Sleep(getRandInt(50, 300));
 
+		t2in = true;
+
 		for (int i = 0; i < getRandInt(100, 1000); ++i) /* some work here */;	// critical section
-		std::cout << "Critical section t2" << std::endl;
+		for (int i = 0; i < 10; ++i) {
+			std::cout << "Critical section t2" << std::endl;
+			if (t1in && t2in)
+				std::cout << "Doesn't work!!!" << std::endl;
+		}
+
+		t2in = false;
 
 		in2.store(false);										// exit protocol
 
 		for (int i = 0; i < getRandInt(100, 1000); ++i) /* some work here */;	// non-critical section
+		for (int i = 0; i < 5; ++i)
 		std::cout << "Non-critical section t2" << std::endl;
 	}
 }
